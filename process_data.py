9b29ea04bc16649719dc48d4b797c6abb31e6547
Paul Nguyen <pnguyen@0584.lan1>
2020-07-09 22:17:44 -0500

    #4 Add day of week sales data processing functionality.
    This commit adds some basic functionality to calculate the total sales, total number of purchases, etc. for each day of the week.
    
    It incluldes functionality to put it in a JSON file.
    
    This commit also includes minor changes to the environment.yml to handle a tqdm dependency.

--- process_data.py
+++ process_data.py
@@ -30,6 +30,8 @@ pandarallel.initialize(nb_workers=mp.cpu_count(), progress_bar=False, verbose=0)
 
 ECOMMERCE_DATA_XLSX_FILE_LOCATION = './data/online_retail_II.xlsx'
 
+OUTPUT_JSON_FILE_LOCATION = './docs/processed_data.json'
+
 #####################################
 # General Data Processing Utilities #
 #####################################
@@ -52,14 +54,44 @@ class CustomEncoder(json.JSONEncoder):
 ##################################################
 
 def clean_ecommerce_data(ecommerce_df: pd.DataFrame) -> pd.DataFrame:
-    ecommerce_df = ecommerce_df[ecommerce_df['Customer ID'] == ecommerce_df['Customer ID']]
+    '''Destructive.'''
+    ecommerce_df = ecommerce_df.rename(columns={'Customer ID': 'CustomerID'}, copy=False) # make column naming convention consistent 
+    ecommerce_df.drop(ecommerce_df[ecommerce_df['CustomerID'] != ecommerce_df['CustomerID']].index, inplace=True)
     ecommerce_df = ecommerce_df.astype({'CustomerID': int}, copy=False)
     assert len(ecommerce_df[ecommerce_df.isnull().any(axis=1)])==0, 'Raw data contains NaN'    
     return ecommerce_df
 
-def generate_output_files(ecommerce_df: pd.DataFrame) -> None:
-    # @todo fill this in
-    return
+def process_ecommerce_data_rowwise(ecommerce_df: pd.DataFrame) -> pd.DataFrame:
+    '''Destructive.'''
+    ecommerce_df['InvoiceTotal'] = ecommerce_df.parallel_apply(lambda row: row.Quantity * row.Price, axis=1)
+    return ecommerce_df
+
+def generate_day_of_week_dict(ecommerce_df: pd.DataFrame) -> dict:
+    day_index_to_string_map = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+    ecommerce_df = ecommerce_df.copy(deep=False)
+    ecommerce_df['DayOfWeek'] = ecommerce_df.InvoiceDate.parallel_map(datetime.datetime.weekday)
+    day_of_week_df = ecommerce_df.groupby('DayOfWeek').agg({
+        'Invoice': 'nunique', 
+        'StockCode': 'nunique',
+        'Quantity': 'sum',
+        'InvoiceTotal': 'sum',
+        'CustomerID': 'nunique',
+    }).rename(columns={
+        'Invoice': 'UniqueInvoiceCount', 
+        'StockCode': 'UniquePurchasedItemCount',
+        'Quantity': 'TotalQuantity',
+        'InvoiceTotal': 'TotalPurchaseAmount',
+        'CustomerID': 'UniqueCustomerCount', 
+    }, copy=False)
+    day_of_week_dict = {day_index_to_string_map[day_of_week_index]:data for day_of_week_index, data in day_of_week_df.to_dict(orient='index').items()}
+    return day_of_week_dict
+
+def generate_output_dict(ecommerce_df: pd.DataFrame) -> dict:
+    day_of_week_data = generate_day_of_week_dict(ecommerce_df)
+    output_dict = {
+        'day_of_week_data': day_of_week_data,
+    }
+    return output_dict
 
 ##########
 # Driver #
@@ -69,7 +101,10 @@ def generate_output_files(ecommerce_df: pd.DataFrame) -> None:
 def process_data() -> None:
     ecommerce_df = pd.read_excel(ECOMMERCE_DATA_XLSX_FILE_LOCATION)
     ecommerce_df = clean_ecommerce_data(ecommerce_df)
-    generate_output_files(ecommerce_df)
+    ecommerce_df = process_ecommerce_data_rowwise(ecommerce_df)
+    output_dict = generate_output_dict(ecommerce_df)
+    with open(OUTPUT_JSON_FILE_LOCATION, 'w') as file_handle:
+        json.dump(output_dict, file_handle, indent=4)
     return
 
 if __name__ == '__main__':
